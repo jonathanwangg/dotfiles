1e0925fa0d82e6a4e189c73ca1f7e881924db435
Jonathan Wang <wangjonathan@microsoft.com>
2025-04-22 14:23:48 -0700

    Remove explicit dependency on 'fzf'

--- nvim/lua/plugins/fzf-lua.lua
+++ nvim/lua/plugins/fzf-lua.lua
@@ -3,7 +3,8 @@
 return {
   "ibhagwan/fzf-lua",
   -- optional for icon support
-  dependencies = { "nvim-tree/nvim-web-devicons" },
+  -- dependencies = { "junegunn/fzf" },
+  dependencies = { },
   -- or if using mini.icons/mini.nvim
   -- dependencies = { "echasnovski/mini.icons" },
   opts = {}
